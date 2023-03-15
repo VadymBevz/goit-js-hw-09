0bb36eed29d9a46799a44a5498a49c0b3574295f
VadymBevz <116199269+VadymBevz@users.noreply.github.com>
2023-03-15 08:40:28 +0000

    Deploying to gh-pages from @ VadymBevz/goit-js-hw-09@e3dee30c74d83fb38ebb31045037afcfbaa3139e 🚀

---
+++ 01-color-switcher.5f55cafa.js
@@ -0,0 +1,2 @@
+var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.5f55cafa.js.map
