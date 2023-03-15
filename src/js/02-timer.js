756188e682c2c72020a173744fc4da87ebdb2dad
VadymBevz <vbevzv@gmail.com>
2023-03-15 11:28:04 +0200

    homework 2-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,49 +1,14 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-const datetimePicker = document.getElementById("datetime-picker");
-const startButton = document.querySelector("[data-start]");
-const daysElement = document.querySelector("[data-days]");
-const hoursElement = document.querySelector("[data-hours]");
-const minutesElement = document.querySelector("[data-minutes]");
-const secondsElement = document.querySelector("[data-seconds]");
-
-let countdown;
-
-function startCountdown() {
-  const selectedDate = new Date(datetimePicker.value);
-  if (selectedDate <= new Date()) {
-    window.alert("Please choose a date in the future");
-    return;
-  }
-  startButton.disabled = true;
-  countdown = setInterval(() => {
-    const totalSeconds = Math.floor((selectedDate - new Date()) / 1000);
-    if (totalSeconds < 0) {
-      clearInterval(countdown);
-      return;
-    }
-    const days = Math.floor(totalSeconds / 86400);
-    const hours = Math.floor((totalSeconds % 86400) / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = Math.floor(totalSeconds % 60);
-    daysElement.textContent = days < 10 ? `0${days}` : days;
-    hoursElement.textContent = hours < 10 ? `0${hours}` : hours;
-    minutesElement.textContent = minutes < 10 ? `0${minutes}` : minutes;
-    secondsElement.textContent = seconds < 10 ? `0${seconds}` : seconds;
-  }, 1000);
-}
-
-flatpickr(datetimePicker, {
+const datePicker = flatpickr("#datetime-picker", {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
-    if (selectedDate <= new Date()) {
+    if (selectedDate < new Date()) {
       window.alert("Please choose a date in the future");
       startButton.disabled = true;
     } else {
@@ -52,5 +17,38 @@ flatpickr(datetimePicker, {
   },
 });
 
-startButton.addEventListener("click", startCountdown);
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+let countdown;
+
+startButton.addEventListener('click', () => {
+  const selectedDate = datePicker.selectedDates[0];
+  countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = selectedDate.getTime() - now;
+
+    if (distance < 0) {
+      clearInterval(countdown);
+      daysElement.textContent = '00';
+      hoursElement.textContent = '00';
+      minutesElement.textContent = '00';
+      secondsElement.textContent = '00';
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, '0');
+
+    daysElement.textContent = days;
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+  }, 1000);
+});
 
