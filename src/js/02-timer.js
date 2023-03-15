485f6c47ef49d88bac9df7abbf3d47d6e9988d85
VadymBevz <vbevzv@gmail.com>
2023-03-15 11:17:37 +0200

    home2

---
+++ src/js/02-timer.js
@@ -0,0 +1,56 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const datetimePicker = document.getElementById("datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const daysElement = document.querySelector("[data-days]");
+const hoursElement = document.querySelector("[data-hours]");
+const minutesElement = document.querySelector("[data-minutes]");
+const secondsElement = document.querySelector("[data-seconds]");
+
+let countdown;
+
+function startCountdown() {
+  const selectedDate = new Date(datetimePicker.value);
+  if (selectedDate <= new Date()) {
+    window.alert("Please choose a date in the future");
+    return;
+  }
+  startButton.disabled = true;
+  countdown = setInterval(() => {
+    const totalSeconds = Math.floor((selectedDate - new Date()) / 1000);
+    if (totalSeconds < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    const days = Math.floor(totalSeconds / 86400);
+    const hours = Math.floor((totalSeconds % 86400) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    daysElement.textContent = days < 10 ? `0${days}` : days;
+    hoursElement.textContent = hours < 10 ? `0${hours}` : hours;
+    minutesElement.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    secondsElement.textContent = seconds < 10 ? `0${seconds}` : seconds;
+  }, 1000);
+}
+
+flatpickr(datetimePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      window.alert("Please choose a date in the future");
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+});
+
+startButton.addEventListener("click", startCountdown);
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+  resolve({ position, delay });
   } else {
-    // Reject
+  reject({ position, delay });
   }
-}
+  }, delay);
+  });
+  }
+  
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+  const promises = [];
+  
+  for (let i = 0; i < amount; i++) {
+  const position = i + 1;
+  const promise = createPromise(position, delay + step * i);
+  promises.push(promise);
+  }
+  
+  Promise.all(promises)
+  .then((results) => {
+  console.log('All promises fulfilled:', results);
+  })
+  .catch((error) => {
+  console.log('At least one promise rejected:', error);
+  });
+  });
